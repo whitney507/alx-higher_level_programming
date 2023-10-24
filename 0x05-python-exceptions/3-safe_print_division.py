093b2f7035f16dfceb29983bd99367e649135f77
whitney507 <whitneymarian507@gmail.com>
2023-10-24 01:18:40 -0700

    updated

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that divides 2 integers and prints the result"""
+
+
+def safe_print_division(a, b):
+    try:
+        division = a / b
+    except (TypeError, ZeroDivisionError):
+        division = None
+    finally:
+        print("Inside result: {}".format(division))
+    return (division)
